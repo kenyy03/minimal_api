4a9e910c4bc8632c155e9ed34ebf1ccd7706a612
Keny Travanino <keny03@unitec.edu>
2025-04-02 20:40:16 -0600

    added: extensions folder

---
+++ Infrastructure/Uow/Extensions/RegisterUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Uow.Extensions
+{
+    public static class RegisterUnitOfWork
+    {
+        public static void AddUnitOfWorkFactory(this IServiceCollection services, Action<IUnitOfWorkFactory, IServiceProvider> configure)
+        {
+            services.AddScoped((Func<IServiceProvider, IUnitOfWorkFactory>)delegate (IServiceProvider resolver)
+            {
+                UnitOfWorkFactory unitOfWorkFactory = new();
+                configure(unitOfWorkFactory, resolver);
+                return unitOfWorkFactory;
+            });
+        }
+    }
+}
