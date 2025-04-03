106fa1b954583b88bca40ee57deacd1da3718db3
Keny Travanino <keny03@unitec.edu>
2025-04-02 20:40:02 -0600

    added: dbcontext setup

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+
+        public DbSet<Ability> Abilities { get; set; }
+    }
+}
